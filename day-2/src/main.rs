241b733859a00228fc99b52e6b066fb446b43119
eulegang <eulegang@gmail.com>
2019-12-03 19:28:34 -0600

    Added day 2 part 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,9 +1,16 @@
+use machine::IntCode;
 use utils::{input, split};
 
 fn main() {
-    let mut buf = split(input(), ",");
+    let buf = split(input(), ",");
 
-    let mut machine = machine::IntCode::new(&mut buf);
+    part2(&buf);
+}
+
+#[allow(dead_code)]
+fn part1(bin: &Vec<u32>) {
+    let mut buf = bin.clone();
+    let mut machine = IntCode::new(&mut buf);
 
     machine[1] = 12;
     machine[2] = 2;
@@ -11,4 +18,23 @@ fn main() {
     machine.run();
 
     println!("{:?}", machine[0]);
+}
+
+#[allow(dead_code)]
+fn part2(bin: &Vec<u32>) {
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut buf = bin.clone();
+            let mut machine = IntCode::new(&mut buf);
+
+            machine[1] = noun;
+            machine[2] = verb;
+
+            machine.run();
+
+            if machine[0] == 19690720 {
+                println!("Answer found: {}", noun * 100 + verb);
+            }
+        }
+    }
 }
